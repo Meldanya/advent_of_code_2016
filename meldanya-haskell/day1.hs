94a7e89007890b97caf3228e4f651aaec16bb93d
Erik Jansson <erikjansson90@gmail.com>
2016-12-03 13:49:35 +0100

    Add Haskell solution for part 1 of day 1.

--- meldanya-haskell/day1.hs
+++ meldanya-haskell/day1.hs
@@ -1,7 +1,10 @@
 import Data.Char
 import Data.List
 
-input = "R5, L5, R5, R3"
+import Input
+
+type PosDir = (Int, Int, Int)
+type Move = (Int, Int)
 
 dist :: String -> Int
 dist = read . filter isNumber
@@ -11,13 +14,29 @@ turn dir
     | "R" `isInfixOf` dir = 1
     | otherwise = -1
 
+split :: String -> [String]
 split = words . filter (/= ',')
 
-combine inp = zip (map turn inp) (map dist inp)
+recombine :: [String] -> [Move]
+recombine inp = zip (map turn inp) (map dist inp)
 
-{- walk (1, dist) pos = (dir + fst pos, -}
+-- Modulo as in Python.
+mod_ :: Int -> Int -> Int
+mod_ a n = ((a `mod` n) + n) `mod` n
+
+next :: Int -> Int -> Int
+next cur dir = (cur + dir) `mod_` 4
+
+walk :: PosDir -> Move -> PosDir
+walk (0, x, y) (dir, dist) = (next 0 dir, x + dir * dist,       y       )
+walk (1, x, y) (dir, dist) = (next 1 dir,       x       , y - dir * dist)
+walk (2, x, y) (dir, dist) = (next 2 dir, x - dir * dist,       y       )
+walk (3, x, y) (dir, dist) = (next 3 dir,       x       , y + dir * dist)
+
+taxi_dist :: PosDir -> Int
+taxi_dist (_, x, y) = abs x + abs y
 
 main = do
-    let dirs = combine $ split input
-
-    print dirs
+    let end = foldl walk (0, 0, 0) (recombine $ split input)
+    let res = taxi_dist end
+    print res
